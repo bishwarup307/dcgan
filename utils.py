1a9f4ffb9967326c7960b42784d8c7ad2ba398ff
bishwarup307 <write2bishwarup@gmail.com>
2020-11-15 20:17:44 +0530

    add fid and inception score to utils

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import glob
 import os
+from typing import Optional
 
 import numpy as np
+import scipy.linalg
 import torch
+import torchvision
 
 
 def gen_noise(batch_size: int, noise_dim: int, device: str):
@@ -104,6 +107,95 @@ class ModelCheckpoint:
             torch.save(
                 model.state_dict(), os.path.join(self.path, f"gen_epoch_{step}.pth")
             )
+
+
+def preprocess_inception(img):
+    img = torch.nn.functional.interpolate(
+        img, size=(299, 299), mode="bilinear", align_corners=False
+    )
+    return img
+
+
+def load_inception_model(
+    pretrained: Optional[bool] = True,
+    weights: Optional[str] = None,
+    device: str = "cpu",
+    top: bool = False,
+):
+    if not pretrained and weights is None:
+        raise ValueError(f"`weights_path` must be provided when `pretrained == False`")
+    inception = torchvision.models.inception_v3(
+        pretrained=pretrained, init_weights=False
+    )
+    if not pretrained:
+        inception.load_state_dict(torch.load(weights))
+    if not top:
+        inception.fc = torch.nn.Identity()
+    inception = inception.to(device)
+    return inception
+
+
+def get_inception_features(model: torch.nn.Module, batch: torch.Tensor):
+    model.eval()
+    batch = preprocess_inception(batch)
+    features = model(batch.detach()).cpu()
+    return features
+
+
+def matrix_sqrt(x):
+    """
+    Function that takes in a matrix and returns the square root of that matrix.
+    For an input matrix A, the output matrix B would be such that B @ B is the matrix A.
+    Parameters:
+        x: a matrix
+    """
+    y = x.cpu().detach().numpy()
+    y = scipy.linalg.sqrtm(y)
+    return torch.Tensor(y.real, device=x.device)
+
+
+def fid(
+    mu_x: torch.Tensor, mu_y: torch.Tensor, sigma_x: torch.Tensor, sigma_y=torch.Tensor
+):
+    return (mu_x - mu_y).norm(2) + (
+        sigma_x + sigma_y - 2 * matrix_sqrt(sigma_x @ sigma_y)
+    )
+
+
+def inception_score(batch: torch.Tensor, eps=1e-6):
+    py = batch.mean(dim=0)
+    kld = batch * (torch.log(batch + eps) - torch.log(py + eps))
+    score = kld.sum(dim=1).mean(dim=0).exp()
+    return score
+
+
+# def fid(
+#     batch_1: torch.Tensor,
+#     batch_2: torch.Tensor,
+#     cache_dir: Optional[str] = None,
+#     pretrained: Optional[bool] = False,
+#     weights_path: Optional[str] = None,
+#     device: Optional[str] = "cpu",
+#     batch_size: Optional[int] = 16,
+# ):
+#     if not pretrained and weights_path is None:
+#         raise ValueError(f"`weights_path` must be provided when `pretrained == False`")
+#
+#     if cache_dir:
+#         os.makedirs(cache_dir, exist_ok=True)
+#
+#     device = torch.device(device)
+#
+#     inception = torchvision.models.inception_v3(
+#         pretrained=pretrained, init_weights=False
+#     )
+#     if not pretrained:
+#         inception.load_state_dict(torch.load(weights_path))
+#     inception = inception.to(device)
+#     inception.eval()
+#     inception.fc = torch.nn.Identity()
+#
+#     features_batch_1, features_batch_2 = [], []
 
 
 if __name__ == "__main__":
